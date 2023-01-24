9aefa2525bb523cea946ae1cc8d3a60fd9bffde1
Jiya Garg <100217150+Jiya-garg9971@users.noreply.github.com>
2023-01-24 08:12:36 +0530

    Create 2) Increasing Triplet Sequence.cpp

---
+++ Adobe/2) Increasing Triplet Sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        //greedy approach
+        int num1=INT_MAX, num2= INT_MAX;
+        for(auto i:nums){
+            if(i<=num1){
+                num1=i;
+            }
+            else if(i<=num2)num2=i;
+            else return true;
+        }
+        return false;
+    }
+};
